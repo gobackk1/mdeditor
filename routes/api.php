284f69be2d491120525c37c4fb4e1895c24326dc
vagrant <vagrant@localhost.localdomain>
2020-01-08 08:01:33 +0900

    Revert "apiのミドルウェアを変更"
    This reverts commit 50581c49002076d9c620b1de4c0201268732bbf8.
    
    apiのミドルウェアを変更

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,24 @@ use Illuminate\Http\Request;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::group(['middleware' => ['api']],function(){
+  Route::post('userdata','ApiController@userdata');
+  Route::post('create/category', 'CategoryController@create');
+  Route::post('update/category', 'CategoryController@update');
+  Route::post('delete/category', 'CategoryController@delete');
+  Route::post('create/memo', 'MemoController@create');
+  Route::post('update/memo', 'MemoController@update');
+  Route::post('delete/memo', 'MemoController@delete');
+  Route::post('clear/trash', 'MemoController@clear_trash');
+  Route::post('test', 'MemoController@test');
+
+  Route::post('/register', 'Auth\RegisterController@register')->name('register');
+  Route::post('/login', 'Auth\LoginController@login')->name('login');
+  Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+
+});
+Route::get('/user', function () {
+    return Auth::user();
+})->name('user');
--- routes/web.php
+++ routes/web.php
@@ -16,24 +16,3 @@ Route::get('/{any?}', 'IndexController@index')->where('any','.+');
 // Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
-Route::group(['middleware' => ['api']], function(){
-  // 初回のデータを返す
-  Route::post('userdata','ApiController@userdata');
-
-  Route::post('create/category', 'CategoryController@create');
-  Route::post('update/category', 'CategoryController@update');
-  Route::post('delete/category', 'CategoryController@delete');
-
-  Route::post('create/memo', 'MemoController@create');
-  Route::post('update/memo', 'MemoController@update');
-  Route::post('delete/memo', 'MemoController@delete');
-  Route::post('clear/trash', 'MemoController@clear_trash');
-
-  Route::post('/register', 'Auth\RegisterController@register');
-  Route::post('/login', 'Auth\LoginController@login');
-  Route::post('/logout', 'Auth\LoginController@logout');
-});
-
-Route::get('/user', function () {
-    return Auth::user();
-})->name('user');
